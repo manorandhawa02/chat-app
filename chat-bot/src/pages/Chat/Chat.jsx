17d869c4bf6d4fdc367fa7224a49000b5274d944
Emaan Randhawa <emaanrandhawa002@gmail.com>
2026-07-15 10:34:39 +0500

    creating chat box component and styling it. Also added a chat-container class to the Chat.css file to style the chat container.

--- chat-bot/src/pages/Chat/Chat.jsx
+++ chat-bot/src/pages/Chat/Chat.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 import './Chat.css'
 const Chat = () => {
   return (
-    <div>
+    <div name='chat'>
+      <div className='chat-container'>
+
+      </div>
       
     </div>
   )
