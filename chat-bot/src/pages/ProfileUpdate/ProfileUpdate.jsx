69d7d5350ddd9702475ca3e1b81cd51d2744b0b9
Emaan Randhawa <emaanrandhawa002@gmail.com>
2026-08-25 12:26:06 +0500

    profile page updated with image preview functionality.

--- chat-bot/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ chat-bot/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, {useState} from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 const ProfileUpdate = () => {
+
+  const [image, setImage] = useState(false);
   return (
-    <div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden/>
+            <img src={ image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+            upload profile picture
+          </label>
+          <input type="text" placeholder='Your Name' required/>
+          <textarea placeholder='About You' required></textarea>
+          <button type='submit'>Update Profile</button>
+        </form>
+        <img className='profile-pic' src={ image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+      </div>
       
     </div>
   )
